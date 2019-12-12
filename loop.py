c9428e008c3a95364d7052734369388690029b53
blrk <blradhakrishnan@gmail.com>
2019-12-12 20:54:14 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,3 @@
+""" Write a loop that outputs the numbers in a list named salaries . 
+The outputs should be formatted in a column that is right-justified, with a
+field width of 12 and a precision of 2. """
