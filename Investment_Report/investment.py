750d5e42f40e9ab5016e23bbb0eb7d0a13a48d1e
blrk <blradhakrishnan@gmail.com>
2019-12-12 21:25:55 +0530

    Create investment.py

---
+++ Investment_Report/investment.py
@@ -0,0 +1,11 @@
+# Accept the inputs
+# read int value x=int(input("Give the value of x"))
+
+# Convert the rate to a decimal number
+
+# Initialize the accumulator for the interest
+
+# Display the header for the table hint: use format string
+
+# Compute and display the results for each year hint: use format string where ever applicable 
+
